d914bcd2a86d7ab7b73c289261c4fd18739bf55d
Jorge Basilio <J.l.Basilio55@gmail.com>
2024-07-02 11:56:59 -0400

    comments and escape sequence

--- Comment_multiLine/newFile.c
+++ Comment_multiLine/newFile.c
@@ -3,5 +3,21 @@
 int main()
 {
     // This is a comment
+
+    /*
+        This is a multiline comment
+        anything after forward slash star and before start back slash
+    */
+
+   /*escape sequence =  character combination consisting of backslash \
+                        followed by a letter or a combination of digits
+                        they specify actions within a line or a string of text.
+                        \n = new line
+                        \t = tab
+   */
+    printf("hello\nworld\n");
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    printf("\"I like pizza - Abraham Lincoln probably\"");
+    return 0;
 }
 
