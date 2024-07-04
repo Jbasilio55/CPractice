681ce4ba52cc7e937cbc899ec21d632c52d76039
Jbasilio55 <j.l.basilio55@gmail.com>
2024-07-04 15:46:41 -0400

    age verification

---
+++ IfStatements/age.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+
+    printf("\nWhat is your age? ");
+    scanf("%d", &age);
+
+    if(age <= 0)
+    {
+        printf("\nSorry you have not been born yet!\n");
+    }
+    else if(age < 21)
+    {
+        printf("\nUnfortunately you are unable to drink!\n");
+    }
+    else if(age > 70)
+    {
+        printf("\nYou can drink but will not advise it because you are too old!\n");
+    }else
+    {
+        printf("\nYou are legally able to drink!\n");
+    }
+}
