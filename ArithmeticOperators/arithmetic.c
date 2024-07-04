c072dcee0a24aff210cc040f2476d5da564f3069
Jbasilio55 <j.l.basilio55@gmail.com>
2024-07-04 00:06:47 -0400

    arithmetic

--- ArithmeticOperators/arithmetic.c
+++ ArithmeticOperators/arithmetic.c
@@ -2,5 +2,45 @@
 
 int main()
 {
-    
+    // arithmetic operators
+
+    // + (addition)
+    // - (subtraction)
+    // * (multiplication)
+    // / (division)
+    // % (modulus)
+    // ++ increment
+    // -- decrement
+
+    int x = 5;
+    int y = 2;
+
+    int z = x + y;
+    printf("%d\n", z);
+
+    z = x - y;
+    printf("%d\n", z);
+
+    z = x * y;
+    printf("%d\n", z);
+
+    float a = x / (float) y;
+    printf("%f\n", a);
+
+    z = x % y;
+    printf("%d\n", z);
+
+    z = x++ + y;
+    printf("%d\n", z);
+
+    z = ++x + y;
+    printf("%d\n", z);
+
+    z = x + y--;
+    printf("%d\n", z);
+
+    z = x + --y;
+    printf("%d\n", z);
+
+    return 0;
 }
