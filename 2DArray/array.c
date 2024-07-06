514c0e0d0b58c7b89f7111bff8fd0f22de18b5f9
Jbasilio55 <j.l.basilio55@gmail.com>
2024-07-05 21:47:06 -0400

    2D array practice and finding length of array

---
+++ 2DArray/array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    //  2D array =  an array, where each element is an entire array
+    //              useful if you need a matrix, grid, or table of data
+
+    int numbers[3][3] = {
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+                        };
+
+    int rows = sizeof(numbers)/sizeof(numbers[0]);
+    int col = sizeof(numbers[0])/sizeof(numbers[0][0]);
+
+    printf("%d\n", rows);
+    printf("%d\n", col);
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            printf("%d", numbers[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
